523df428bc4ab3ded27ff93defad13dd6d2cba7f
William Mbotta <sergembotta@mailoo.org>
2020-06-05 23:18:55 +0200

    from async to sync

--- Database.ts
+++ Database.ts
@@ -1,9 +1,9 @@
 import * as crypto from "crypto";
 import { join, parse } from "path";
-import { deflate } from "zlib";
+import { deflateSync } from "zlib";
 import blob from "./blob";
 import * as mkdirp from "mkdirp";
-import { writeFile } from "fs";
+import { writeFileSync } from "fs";
 
 interface Database {
   pathname: string;
@@ -13,7 +13,7 @@ class Database {
   constructor(pathname: string) {
     this.pathname = pathname;
   }
-  store = async (object: blob) => {
+  store = (object: blob) => {
     const str = object.toBuffer();
     const content = Buffer.concat([
       Buffer.from(`${object.type}${str.length}\0`),
@@ -22,19 +22,21 @@ class Database {
     const shasum = crypto.createHash("sha1");
     shasum.update(content);
     object.oid = shasum.digest("hex");
-    await this.writeObject(object.oid, content);
-  };
-  private writeObject = async (oid: string, content: Buffer): Promise<void> => {
-    console.log("pathname", this.pathname);
-    const object_path = join(this.pathname, oid.slice(0, 2), oid.slice(2, -2));
-    const dirname = parse(object_path).dir;
-    console.log("DIRNAME", dirname);
-    await mkdirp(dirname);
-    console.log(content, "bufffffffffffffffffffffff");
-    deflate(content, (_, buffer) => {
-      writeFile(object_path, buffer, () => console.log("done", dirname));
-    });
+    writeObject(object.oid, content, this.pathname);
   };
 }
-
+function writeObject(oid: string, content: Buffer, pathname) {
+  //console.log("pathname", pathname);
+  const object_path = join(pathname, oid.slice(0, 2), oid.slice(2, -2));
+  const dirname = parse(object_path).dir;
+  console.log("DIRNAME", dirname);
+  mkdirp.sync(dirname);
+  //console.log(content, "bufffffffffffffffffffffff");
+  console.log("OID:", oid);
+  //console.log("OTDOOOOO", dirname);
+  const buffer = deflateSync(content);
+  //console.log("inside deflate ");
+  writeFileSync(object_path, buffer);
+  //console.log("done", dirname);
+}
 export default Database;
--- git.ts
+++ git.ts
@@ -24,39 +24,37 @@ import Commit from "./commit";
       console.info(`Initialized empty Jit repository in ${gitPath}`);
       process.exit(0);
     }
-    case "commit":
-      {
-        const dbPath = join(gitPath, "objects");
-        const workSpace = new WorkSpace(rootPath);
-        const database = new Database(dbPath);
-        let entries: Entry[] = [];
+    case "commit": {
+      const dbPath = join(gitPath, "objects");
+      const workSpace = new WorkSpace(rootPath);
+      const database = new Database(dbPath);
+      let entries: Entry[] = [];
 
-        for (const file of workSpace.listFiles()) {
-          const data = workSpace.readFile(file);
-          const bloB = new blob(data);
+      for (const file of workSpace.listFiles()) {
+        const data = workSpace.readFile(file);
+        const bloB = new blob(data);
 
-          await database.store(bloB);
+        await database.store(bloB);
 
-          entries.push(new Entry(file, bloB.oid));
-        }
-
-        const tree = new Tree(entries);
-        database.store(tree);
-
-        const name = process.env?.GIT_AUTHOR_NAME || "sepiropht";
-        const email = process.env?.GIT_AUTHOR_EMAIL || "sergembotta@mailoo.org";
-        const author = new Author(name, email, new Date());
-        const message = fs.readFileSync("/dev/stdin").toString();
-        const commit = new Commit(tree.oid, author, message);
-        database.store(commit);
-        const Head = join(gitPath, "HEAD");
-
-        fs.writeFile(Head, commit.oid, () => {
-          console.log(`[(root-commit) ${commit.oid}] ${message}`);
-          process.exit(0);
-        });
+        entries.push(new Entry(file, bloB.oid));
       }
-      break;
+
+      const tree = new Tree(entries);
+      database.store(tree);
+
+      const name = process.env?.GIT_AUTHOR_NAME || "sepiropht";
+      const email = process.env?.GIT_AUTHOR_EMAIL || "sergembotta@mailoo.org";
+      const author = new Author(name, email, new Date());
+      const message = fs.readFileSync("/dev/stdin").toString();
+      const commit = new Commit(tree.oid, author, message);
+
+      database.store(commit);
+      const Head = join(gitPath, "HEAD");
+
+      fs.writeFileSync(Head, commit.oid);
+      console.log(`[(root-commit) ${commit.oid}] ${message}`);
+      process.exit(0);
+    }
     default: {
       console.error(`jit: '${command}' is not a jit command.`);
       process.exit(1);
