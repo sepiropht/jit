b138002dd068b9d168ea5c706cca2504fd1dffdc
William Mbotta <sergembotta@mailoo.org>
2020-06-08 23:14:41 +0200

    get all files and directory recursively

--- workspace.ts
+++ workspace.ts
@@ -1,5 +1,5 @@
 import * as fs from "fs";
-import { join } from "path";
+import { join, relative } from "path";
 
 interface Workspace {
   pathname: string;
@@ -11,16 +11,35 @@ class Workspace {
     this.ignore = [".", "..", ".jit", ".git", "node_modules"];
   }
   listFiles(): string[] {
-    return fs
-      .readdirSync(this.pathname)
-      .filter((file) => !this.ignore.includes(file));
+    return this.getAllFiles(this.pathname, []).filter(
+      (file) => !this.ignore.includes(file)
+    );
   }
-  readFile(path): Buffer {
+  readFile(path: string): Buffer {
     return fs.readFileSync(join(this.pathname, path));
   }
 
   getMode(file: string): number {
     return parseInt(fs.statSync(file).mode.toString(8), 10);
+  }
+  getAllFiles(dirPath: string, arrayOfFiles: Array<string>) {
+    const files = fs.readdirSync(dirPath);
+
+    arrayOfFiles = arrayOfFiles || [];
+
+    files.forEach((file) => {
+      if (this.ignore.includes(file)) {
+        return;
+      }
+      if (fs.statSync(join(dirPath, file)).isDirectory()) {
+        arrayOfFiles = this.getAllFiles(join(dirPath, file), arrayOfFiles);
+      } else {
+        const relativePath = relative(this.pathname, dirPath);
+        arrayOfFiles.push(join(relativePath, file));
+      }
+    });
+
+    return arrayOfFiles;
   }
 }
 
